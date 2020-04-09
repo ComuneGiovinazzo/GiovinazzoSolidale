ae03f44c122fbb6b8ed89e0afe1225142f9e9360
LorenzoZaccagnini <lorenzozcg@gmail.com>
2020-04-09 14:53:47 +0200

    removed test endpoint

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 
 http.createServer(function (req, res) {
-  if (req.method === 'GET' && req.url === '/test') {
-    console.log("test ok 3");
-  }
   handler(req, res, function (err) {
     res.statusCode = 404
     res.end('no such location')
